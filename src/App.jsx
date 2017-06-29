d184c44c2f0261ad1852cf2f44dcf66227247d7b
DESKTOP-2OU1GPU\nahid <nahiduzzaman@sgcsoft.net>
2017-06-30 00:08:11 +0600

    music master final

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,57 @@ import React,{Component} from 'react';
 import './App.css';
 import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 import Profile from './Profile';
+import Gallery from './Gallery';
+
+
 class App extends Component{
     constructor(props){
-    	super(props);
-    	this.state = {
-        query:'',
-        artist:null,
-        tracks:null
-      };
+        super(props);
+        this.state = {
+            query:'',
+            artist:null,
+            tracks:[]
+        };
     }
 
     search(){
-      console.log('this.state', this.state);
-      const TOKEN = 'BQCOkBPhmvYrJr7xd5rZ2Xk4rkvZX0CW4b3j5q5UhFXXBf93WulpwSqfM72RIkieyGXWfEAl9JewFXKOpwV0E74988cGeyCLrpU0y34OU_RRzZYKg8oUMLfwxggKVGH466K_RQR7u-wIKmunm023-WwhNjRAfSzlkw';
-      const BASE_URL = 'https://api.spotify.com/v1/search?';
-      const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
-      const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1&access_token='+ TOKEN;
-      console.log('FETCH_URL',FETCH_URL);
-      fetch(FETCH_URL,{
-        method: 'GET'
-      })
-      .then(response => response.json())
-      .then(json => {
-          const artist = json.artists.items[0];
-          console.log('artist',artist);
-          this.setState({artist:artist});
-          const ALBUM_FETCH_URL = ALBUM_URL + artist.id + '/top-tracks?country=US&access_token='+ TOKEN;
-          console.log('ALBUM_FETCH_URL',ALBUM_FETCH_URL);
-          fetch(ALBUM_FETCH_URL,{
-            method: 'GET'
-          })
-          .then(response => response.json())
-          .then(json => {
-            console.log('artist\'s top-tracks',json);
-            const { tracks } = json;
-            this.setState({tracks:tracks}); // this line can be re-written as 'this.setState({tracks})', because key and value has same name!  
-          })
-      });
+        console.log('this.state', this.state);
+        const GET_TOKEN_URL = 'http://localhost:8888/refresh_token?refresh_token=AQBYGrolFwxTUzM57sjSLPZMT_jZCLDN7iykByFoIyAc_-Ky1CE4upwVgxNAOOOwHdvidLEghUnnjjvJOV4H1M8645Gp9piqUQ8LKVr-X5QQj6HAgFILt9pYw5KddyfI9c0';
+        fetch(GET_TOKEN_URL,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log('parsed json', json) // access json.body here
+            const TOKEN = json.access_token;
+            const BASE_URL = 'https://api.spotify.com/v1/search?';
+            const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+            const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1&access_token='+ TOKEN;
+            console.log('FETCH_URL',FETCH_URL);
+            fetch(FETCH_URL,{
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => {
+                const artist = json.artists.items[0];
+                console.log('artist',artist);
+                this.setState({artist:artist});
+                const ALBUM_FETCH_URL = ALBUM_URL + artist.id + '/top-tracks?country=US&access_token='+ TOKEN;
+                console.log('ALBUM_FETCH_URL',ALBUM_FETCH_URL);
+                fetch(ALBUM_FETCH_URL,{
+                    method: 'GET'
+                })
+                .then(response => response.json())
+                .then(json => {
+                    console.log('artist\'s top-tracks',json);
+                    const { tracks } = json;
+                    this.setState({tracks}); // this line can be re-written as 'this.setState({tracks})', because key and value has same name!
+                })
+            })
+        });
 
 
     }
@@ -47,34 +61,34 @@ class App extends Component{
             <div className="app">
                 <div className="app-title">Music Master</div>
                 <FormGroup>
-                  <InputGroup>
-                    <FormControl
-                      type="text"
-                      placeholder="Search for an Artist"
-                      value={this.state.query}
-                      onChange={event => this.setState({query : event.target.value})}
-                      onKeyPress={event => {
-                        if(event.key === 'Enter'){
-                          this.search();
-                        }
-                      }}
-                    />
-                    <InputGroup.Addon onClick={()=>this.search()}>
-                      <Glyphicon glyph="search"></Glyphicon>
-                    </InputGroup.Addon>
-                  </InputGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for an Artist"
+                            value={this.state.query}
+                            onChange={event => this.setState({query : event.target.value})}
+                            onKeyPress={event => {
+                                if(event.key === 'Enter'){
+                                    this.search();
+                                }
+                            }}
+                        />
+                        <InputGroup.Addon onClick={()=>this.search()}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
                 </FormGroup>
                 {
-                  this.state.artist !== null
-                  ? <div>
-                      <Profile
-                         artist={this.state.artist}
-                      />
-                      <div className="Gallery">
-                        Gallery
-                      </div>
+                    this.state.artist !== null
+                    ? <div>
+                        <Profile
+                            artist={this.state.artist}
+                        />
+                        <Gallery
+                            tracks={this.state.tracks} //tracks is props it will be passed on Gallery Component
+                        />
                     </div>
-                  : <div></div>
+                    : <div></div>
                 }
 
             </div>
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,45 +4,45 @@ class Profile extends Component {
     render() {
         console.log('this.props',this.props);
         let artist = {
-                        name:'',
-                        followers: {
-                                     total:''
-                                   },
-                        images: [
-                                  {
-                                    url:''
-                                  }
-                                ],
-                         genres: []
-                      }
+            name:'',
+            followers: {
+                total:''
+            },
+            images: [
+                {
+                    url:''
+                }
+            ],
+            genres: []
+        }
         if(this.props.artist !== null){
-          artist = this.props.artist;
+            artist = this.props.artist;
         }
         return (
             <div className="profile">
-              <img
-                alt="Profile"
-                className="profile-img"
-                src={artist.images[0].url}
-              />
-              <div className="profile-info">
-                <div className="profile-name">{artist.name}</div>
-                <div className="profile-followers">
-                  {artist.followers.total} followers
+                <img
+                    alt="Profile"
+                    className="profile-img"
+                    src={artist.images[0].url}
+                />
+                <div className="profile-info">
+                    <div className="profile-name">{artist.name}</div>
+                    <div className="profile-followers">
+                        {artist.followers.total} followers
+                    </div>
+                    <div className="profile-genres">
+                        {
+                            artist.genres.map((genre,key) => {
+                                genre = genre === artist.genres[artist.genres.length-1]
+                                ? `& ${genre}`
+                                : `${genre},`;
+                                return(
+                                    <span key={key}>{genre}</span> //solves a warning
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-                <div className="profile-genres">
-                  {
-                    artist.genres.map((genre,key) => {
-                      genre = genre === artist.genres[artist.genres.length-1]
-                                    ? `& ${genre}`
-                                    : `${genre},`;
-                      return(
-                        <span key={key}>{genre}</span> //solves a warning
-                      )
-                    })
-                  }
-                </div>
-              </div>
             </div>
         );
     }
